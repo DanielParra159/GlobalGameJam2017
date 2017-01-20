2175b66aa1c83a416134b70118435778baba9df2
DESKTOP-JJ1A081\Cristian <crisw8mc@gmail.com>
2017-01-20 22:32:13 +0100

    Movimiento

---
+++ Project/Assets/scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Movement : MonoBehaviour {
+    [SerializeField]
+    private float Velocidad;
+
+    Rigidbody rb;
+    
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    } 
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direccionH = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+
+        rb.MovePosition(rb.position + direccionH * Velocidad *  Time.fixedDeltaTime);
+        
+    }
+
+}
