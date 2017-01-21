5d4f7e93d3c146c896481cc59d25829b2ef5badf
DESKTOP-JJ1A081\Cristian <crisw8mc@gmail.com>
2017-01-21 03:33:42 +0100

    correcciones proyectil

--- Project/Assets/Data/Scripts/Proyectil.cs
+++ Project/Assets/Data/Scripts/Proyectil.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Proyectil : MonoBehaviour {
 
     [SerializeField]
-    private GameObject Personaje;
+    private GameObject NuevaOnda;
     private Vector3 Direccion;
-    private Vector3 Personajeposicion;
     [SerializeField]
     private float Speed;
 
-	// Use this for initialization
-	void Start () {
+    private Vector3 UltimaPosicion;
+
+    // Use this for initialization
+    void Start () {
     }
 
     public void Configure(Vector3 dir)
@@ -21,9 +22,23 @@ public class Proyectil : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    private void Update () {
+        Collider[] Colliders;
+        Collider[] ColliderPared;
         gameObject.transform.position += Direccion * Speed * Time.deltaTime;
-
         Destroy(gameObject, 4);
+
+        Colliders = Physics.OverlapSphere(gameObject.transform.position, 1, LayerDefinitions.ENEMY_MASK);
+        ColliderPared = Physics.OverlapSphere(gameObject.transform.position, 1, LayerDefinitions.INVISIBLE_OBSTACLE_LAYER);
+
+        if (Colliders.Length > 0) {
+            Destroy(gameObject);
+        }
+
+        else if (ColliderPared.Length > 0) {
+            UltimaPosicion =  gameObject.transform.position;
+            Destroy(gameObject);
+            Instantiate(NuevaOnda);
+        }
     }
 }
