3f9185e19695c45f4973a8151f2ee5a308fe9da8
DESKTOP-JJ1A081\Cristian <crisw8mc@gmail.com>
2017-01-22 08:21:09 +0100

    ultimo retoque

--- Project/Assets/Data/Scripts/Arma.cs
+++ Project/Assets/Data/Scripts/Arma.cs
@@ -15,9 +15,11 @@ public class Arma : MonoBehaviour
     private bool firstshoot;
     private float ProjectileOffset = 1.3f;
     private Vector3 Direccion;
+    private AudioSource audio;
 
     private void Awake()
     {
+        audio = gameObject.GetComponent<AudioSource>();
         Personaje = gameObject.gameObject;
         fireRate = 0.5f;
     }
@@ -44,7 +46,8 @@ public class Arma : MonoBehaviour
             GameObject pro = Instantiate(ProjectilePrefab,
                         new Vector3(Personaje.transform.position.x, Personaje.transform.position.y + 1, Personaje.transform.position.z),
                         Quaternion.identity);
-                
+
+            
             pro.GetComponent<Proyectil>().Configure(Direccion);
             lastShot = Time.time;
             yield return new WaitForSeconds(BulletFireRate);
